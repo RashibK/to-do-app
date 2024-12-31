300b4cce97bc54aa35b898effca2c29ac986be7e
Rashib Khanal <contact@rashib.dev>
2024-12-31 15:12:26 -0500

    add: PUT and DELETE METHOD to update/delete TODOS

--- backend/to_do/urls.py
+++ backend/to_do/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/', views.ToDosAPI, name='todos-api')
+    path('api/', views.ToDosAPI, name='todos-api'),
+    path('api/<int:pk>', views.editTodos, name='update-todo-api'),
 ]
--- backend/to_do/views.py
+++ backend/to_do/views.py
@@ -5,12 +5,6 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-
-
-
-
-
-
 @api_view(['GET', 'POST'])
 def ToDosAPI(request):
 
@@ -27,5 +21,26 @@ def ToDosAPI(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
     
+   
+
+@api_view(['PUT', 'DELETE'])
+def editTodos(request, pk):
+     # for updating the data that's already in database
+    try: 
+        todo = ToDo.objects.get(pk=pk)
+    except ToDo.DoesNotExist:
+        return Response(status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = ToDoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    
+   # for deleting a todo from the database
+    elif request.method == 'DELETE':
+            todo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
