2443d8c29689fff515b13be1fd0ad010cd2e42cf
Rashib Khanal <contact@rashib.dev>
2025-01-03 18:00:43 -0500

    update: fixed the All To-dos listing css, now it's bearable

--- frontend/src/components/ToDoList.js
+++ frontend/src/components/ToDoList.js
@@ -13,7 +13,7 @@ export const TodoList = () => {
     const [updatedData, setUpdatedData] = useState({});
 
         // for getting all the todos from the database
-    const {data: alltodos, isLoading, isError, error } = useQuery({
+    const {data: alltodos, isLoading, isError, error, isFetched } = useQuery({
             queryKey: ['get-all-todos'],
             queryFn: getTodo,
         
@@ -33,15 +33,16 @@ export const TodoList = () => {
          }
 
     return <div className="all-todos">
-        <p>All To-Dos</p>
+
         {isLoading && <p>...Loading....</p>}
         {isError && <p>{error?.message}</p>}
         <UpdateTodo updatedData={updatedData} setUpdatedData={setUpdatedData} isupdatebtnclicked={isupdatebuttonclicked} setIsupdatebuttonclicked={setIsupdatebuttonclicked} />
+            {isFetched && alltodos.length !== 0 && <p className="all-todos-title">All To-Dos</p>}
             {alltodos && alltodos.map(todo => {
                 return <div className="todo">
                     <div className="todo-content">
-                    <h1>{todo.title}</h1>
-                    <p>{todo.description}</p>
+                    <p className="todo-title">{todo.title}</p>
+                    <p className="todo-description">{todo.description}</p>
                     </div>
                     <div className="todo-buttons" >
                     {/* <button id={todo.id} >Update</button> */}
